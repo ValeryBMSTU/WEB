5569d0f5262e0e71894536222dfd093fc7ad8a7e
ValeryBMSTU <30847068+ValeryBMSTU@users.noreply.github.com>
2019-04-22 16:15:04 +0300

    Views refactoring.

--- askme/asker/urls.py
+++ askme/asker/urls.py
@@ -2,21 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('index/', index, name="index"),
-    path('questions/', questions, name='questions'),
+    path('', index, name="index"),
+    path('questions/', Questions.as_view(), name='questions'),
     path('questions/<int:pk>/', QuestionDetail.as_view(), name='questionDetail'),
-    path('tags/', tags, name='tags'),
+    path('tags/', Tags.as_view(), name='tags'),
     path('tags/<int:pk>/', TagDetail.as_view(), name="tagDetail"),
-
-    path('ask/', ask, name="ask"),
-    path('question/', question, name="question"),
-    path('settings/', settings, name="settings"),
-    path('login/', login, name="login"),
-    path('registration/', registration, name="registration"),
-    path('users/', users, name="users"),
-    # path('questionsByDate/', QuestionByDateView.as_view(), name='questionsByDate'),
-    # path('questionsByRate/', QuestionByRateView.as_view(), name='questionsByRate'),
-    # path('questionsByTag/', QuesStionByTagView.as_view(), name='questionsByTag'),
-    # path('questions/<int:number>/', questions_detail, name='question_detail'),
-    # path('questions/', ListView.as_view(queryset=Question.objects.all().order_by("title")[:10], template_name="asker/questions.html")),
+    path('ask/', Ask.as_view(), name="ask"),
+    path('settings/', Settings.as_view(), name="settings"),
+    path('login/', Login.as_view(), name="login"),
+    path('registration/', Registration.as_view(), name="registration"),
+    path('users/', Users.as_view(), name="users"),
 ]
--- askme/asker/views.py
+++ askme/asker/views.py
@@ -7,73 +7,105 @@ def index(request):
     questions = Question.objects.all()
     return render(request, 'asker/index.html', context = {'questions': questions})
 
-def questions(request):
-    questions = Question.objects.all()
-    return render(request, 'asker/questions.html', context = {'questions': questions})
+# def questions(request):
+#     questions = Question.objects.all()
+#     return render(request, 'asker/questions.html', context = {'questions': questions})
 
 # def questionDetail(request, slug):
 #     question = Question.objects.get(slug__iexact=slug)
 #     return render(request, 'asker/questionDetail.html', context={'question': question})
 
-def tags(request):
-    tags = Tag.objects.all()
-    return render(request, 'asker/tags.html', context={'tags': tags})
+# def tags(request):
+#     tags = Tag.objects.all()
+#     return render(request, 'asker/tags.html', context={'tags': tags})
 
 # def tagDetail(request, pk):
 #     tag = Tag.objects.get(pk=pk)
 #     return render(request, 'asker/tagDetail.html', context={'tag': tag})
 
-def ask(request):
-    return render(
-        request,
-        'asker/ask.html',
-        {"values": ['Какая-то дичь', '8-916-416-86-67']}
-    )
-def question(request):
-    return render(
-        request,
-        'asker/question.html',
-        {"asker": []}
-    )
-def settings(request):
-    return render(
-        request,
-        'asker/settings.html',
-        {"asker": []}
-    )
-def login(request):
-    return render(
-        request,
-        'asker/login.html',
-        {"asker": []}
-    )
-def registration(request):
-    return render(
-        request,
-        'asker/registration.html',
-        {"asker": []}
-    )
-def users(request):
-    return render(
-        request,
-        'asker/users.html',
-        {"asker": []}
-    )
+# def ask(request):
+#     return render(
+#         request,
+#         'asker/ask.html',
+#         {"values": ['Какая-то дичь', '8-916-416-86-67']}
+#     )
 
+# def settings(request):
+#     return render(
+#         request,
+#         'asker/settings.html',
+#         {"asker": []}
+#     )
+# def login(request):
+#     return render(
+#         request,
+#         'asker/login.html',
+#         {"asker": []}
+#     )
+# def registration(request):
+#     return render(
+#         request,
+#         'asker/registration.html',
+#         {"asker": []}
+#     )
+# def users(request):
+#     return render(
+#         request,
+#         'asker/users.html',
+#         {"asker": []}
+#     )
 
-class QuestionDetail(ObjectsDetailMixing, View):
-    model = Question
-    template = 'asker/questionDetail.html'
-    # def get(self, request, pk):
+class Users(view):
+
+    def get(self, request):
+        return render(request, 'asker/users.html', context={})
+
+class Registration(view):
+
+    def get(self, request):
+        return render(request, 'asker/registration.html', context={})
+
+class Login(View):
+
+    def get(self, request):
+        return render(request, 'asker/login.html', context={})
+
+class Settings(View):
+
+    def get(self, request):
+        return render(request, 'asker/settings.html', context={})
+
+class Ask(View):
+
+    def get(self, request):
+        return render(request, 'asker/ask.html', context={})
+
+class Questions(View):
+
+    def get(self, request):
+        questions = Question.objects.order_by("-createDate")
+        return render(request, 'asker/questions.html', context={'questions': questions})
+
+class QuestionDetail(View):
+    # model = Question
+    # template = 'asker/questionDetail.html'
+    def get(self, request, pk):
         # question = Question.objects.get(pk=pk)
-        # question = get_object_or_404(Question, pk=pk)
-        # return render(request, 'asker/questionDetail.html', context={'question': question})
+        question = get_object_or_404(Question, pk=pk)
+        return render(request, 'asker/questionDetail.html', context={'question': question})
 
-class TagDetail(ObjectsDetailMixing, View):
-    model = Tag
-    template = 'asker/tagDetail.html'
-    # def get(self, request, pk):
-    #     tag = Tag.objects.get(pk=pk)
-    #     tag = get_object_or_404(Tag, pk=pk)
-    #     return render(request, 'asker/tagDetail.html', context={'tag': tag})
+class Tags(View):
+
+    def get(self, equest):
+        tags = Tag.objects.all()
+        return render(request, 'asker/tags.html', context={'tags': tags})
+
+
+class TagDetail(View):
+    # model = Tag
+    # template = 'asker/tagDetail.html'
+    def get(self, request, pk):
+        # tag = Tag.objects.get(pk=pk)
+        tag = get_object_or_404(Tag, pk=pk)
+        return render(request, 'asker/tagDetail.html', context={'tag': tag})
 
